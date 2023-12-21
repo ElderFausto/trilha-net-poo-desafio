388d32e2b6ddbb8bcc7f7e41b6a03f7001f5b615
Elder Fausto <elderfavsto@gmail.com>
2023-12-21 13:13:23 -0300

    Desafio POO concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("5581996341285", "Iphone 15 Pro Max", "001970-01-010000-0", 256);
+Console.WriteLine("Iphone 15 Pro Max");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Call of Duty");
+
+Console.WriteLine("\n");
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine("Nokia G60 5G");
+Smartphone nokia = new Nokia("5581990574135", "Nokia G60 5G", "001977-01-018000-0", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Free Fire");
